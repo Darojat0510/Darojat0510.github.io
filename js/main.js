94a0d66bfb2938272deaac5139b46efac66f6c11
Darojat Github <java1847@gmail.com>
2022-02-26 16:28:35 +0700

    Menambah Javascript Di Main.js

--- js/main.js
+++ js/main.js
@@ -199,14 +199,14 @@ popup.scrollTo(0,projectDetailsContainer.offsetTop);
 
 
 
-// (()=>{
-// const sections=document.querySelectorAll(".section");
-// sections.forEach((section)=>{
-//  if(!section.classList.contains("active")){
-//     section.classList.add("hide");
-//  }
-// })
-// })();
+(()=>{
+const sections=document.querySelectorAll(".section");
+sections.forEach((section)=>{
+ if(!section.classList.contains("active")){
+    section.classList.add("hide");
+ }
+})
+})();
 
 
 window.addEventListener("load",()=>{
